b084b1a4c31fa0af046340f7dc1c913140d76fab
ShaikhAaman <51033370+ShaikhAaman@users.noreply.github.com>
2020-04-02 20:01:45 -0400

    Final Project

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.finalproject;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main1);
+
+
+     /*   Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this,
+                    ImageMainActivity.class);
+            startActivity(intent);
+        });*/
+
+        ImageButton nasaImgView = findViewById(R.id.button2);
+        nasaImgView.setOnClickListener(e -> {
+            Intent i = new Intent(this, ImageMainActivity.class);
+            startActivity(i);
+        });
+
+
+
     }
 }
